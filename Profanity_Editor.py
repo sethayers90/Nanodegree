b263eb371c9b0b420f04b0327b0e2a5f7398df59
SETH R AYERS <sethayers90@gmail.com>
2017-03-18 16:11:43 -0400

    Add files via upload

---
+++ Profanity_Editor.py
@@ -0,0 +1,20 @@
+import urllib
+
+def read_text():
+    quotes = open("C:\Users\Seth\Desktop\Nanodegree\Python\Profanity Editor\movie_quotes.txt")
+    contents_of_file = quotes.read()
+    quotes.close()
+    check_profanity(contents_of_file)
+    
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
+    output = connection.read()
+    #print(output)
+    connection.close()
+    if "true" in output:
+        print("Profanity Alert!!")
+    elif "false" in output:
+        print ("This document has no curse words!")
+    else:
+        print ("Could not scan the document properly")
+read_text()
