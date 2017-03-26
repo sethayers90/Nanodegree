eb7eb50ffbb6e1934719bdc5e478e69081265314
SETH R AYERS <sethayers90@gmail.com>
2017-03-26 09:11:32 -0400

    Add files via upload
    Added comments, and fixed the code to make it more compliant

--- media.py
+++ media.py
@@ -1,10 +1,20 @@
 import webbrowser
+'''
+Importing webbrowser to make it so I can use YouTube
+to be able to see the movie trailer.
+Also, creating class Movie to be able to
+store the same values for multiple movies.
+'''
+
 
 class Movie():
-    def __init__(self,movie_title,movie_storyline, poster_image,trailer_youtube):
+    def __init__(self, movie_title,
+                 movie_storyline,
+                 poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
